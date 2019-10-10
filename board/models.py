85af672cee6edf3f27a56747fd6980ae17add677
daminKIM <ekals4407@gmail.com>
2019-10-10 19:26:18 +0900

    gitstudy

--- board/models.py
+++ board/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Board(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    body = models.TextField()   
-    create_date=models.DateTimeField(auto_now_add=True)
-    modify_date=models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -17,8 +18,12 @@ class Board(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey('account.User',on_delete=models.CASCADE, related_name="comment_user")
+    user = models.ForeignKey(
+        'account.User', on_delete=models.CASCADE, related_name="comment_user")
     body = models.TextField(primary_key=True)
-    create_date=models.DateTimeField(auto_now_add=True)
-    modify_date=models.DateTimeField(auto_now=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)
 
+
+class Test(models.Model):
+    title = models.CharField(max_length=100)
